7f63a52c25f94371c0ea050f9821e32eb2a0f088
Daniil <daniil_akimov_2002@mail.ru>
2022-03-28 11:59:15 +0300

    Second commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className={'justify-content-center mt-3'} style={{textAlign: 'center'}}>
+            <h3>Моё первое приложение, созданное при помощи 'WebStorm'</h3>
+            <p>Почта для связи: daniil_akimov_2002@mail.ru</p>
+        </div>
+    );
+};
+
+export default AboutPage;
